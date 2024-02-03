2e35862754dcae1022e48cf9005077a3f84b7133
Uday Sharma <itsudayy@gmail.com>
2024-02-04 04:30:04 +0530

    basic app done

---
+++ app/lib/confessions.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ConfessionPost {
+  final int userID;
+  final String body;
+  final String time;
+
+  ConfessionPost(
+      {required this.userID, required this.body, required this.time});
+}
+
+class Confessions extends StatefulWidget {
+  @override
+  State<Confessions> createState() => ConfessionsState();
+}
+
+class ConfessionsState extends State<Confessions> {
+  bool empty = true;
+  List<ConfessionPost> confessions = [];
+  String develURL = 'http://10.0.2.2:5000/confessions';
+  String deployURL = '';
+
+  Future<http.Response> fetchConfessions() {
+    return http.get(Uri.parse(develURL),
+        headers: {'email': "uday.224026@sggscc.ac.in"});
+  }
+
+  void fetch() async {
+    http.Response res = await fetchConfessions();
+    // Handle the response, update the confessions list or perform other actions.
+    if (res.statusCode == 200) {
+      List<dynamic> data = json.decode(res.body)["confessions"];
+
+      setState(() {
+        empty = false;
+        for (int i = 0; i < data.length; i++) {
+          confessions.add(ConfessionPost(
+              userID: data[i][1], body: data[i][2], time: data[i][3]));
+        }
+      });
+    } else {
+      print("Error");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Call the fetch method when the widget is inserted into the tree.
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (empty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Confessions'),
+        ),
+        body: Text('Confessions Page'),
+      );
+    } else {
+      return Scaffold(
+          appBar: AppBar(title: Text('Confessions')),
+          body: ListView.separated(
+            itemCount: confessions.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Confession(
+                  body: confessions[index].body, time: confessions[index].time);
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ));
+    }
+  }
+}
+
+class Confession extends StatelessWidget {
+  String body;
+  String time;
+
+  Confession({super.key, required this.body, required this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Row(
+          children: [
+            Text(
+              'Anonymous',
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(time)
+          ],
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Text(
+            body,
+            textAlign: TextAlign.left,
+          ),
+        )
+      ]),
+    );
+  }
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:app/confessions.dart';
+import 'package:app/profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List<Widget> pages = [Confessions(), Profile()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Confessions"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_2), label: "Profile")
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
